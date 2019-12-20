769a6aa3e9575f50e83493e026605cf31dc3de60
JA Samitier <jesus.samitier@caseonit.com>
2019-12-20 12:43:17 +0100

    improved content loading
    now it reads markdown files from fs

--- src/routes/blog/_content/index.js
+++ src/routes/blog/_content/index.js
@@ -1,5 +1,15 @@
 'use strict';
+import { filter, sort, reverse, pipe } from 'sanctuary';
+const { readdirSync } = require('fs');
 
-export default [
-    'que-pasa-co'
-];
+const getFiles = (dir) => readdirSync(dir);
+const isMarkdown = (x) => x.includes('.md');
+
+const posts = pipe([
+    getFiles,
+    filter(isMarkdown),
+    sort,
+    reverse
+]);
+
+export default posts('./src/routes/blog/_content/');
--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,7 +1,7 @@
 import { compose, map } from 'sanctuary';
 import postList from './_content/';
 
-const getRawPost = (name) => require(`./_content/${name}.md`);
+const getRawPost = (name) => require(`./_content/${name}`);
 const formatPost = ({ attributes: { title, slug }, html }) => ({ title, slug, html });
 const getPost = compose(formatPost)(getRawPost);
 
