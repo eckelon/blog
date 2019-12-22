be120f77912dc8df7f07fa6a66b652a0519a9bd3
JA Samitier <jesus.samitier@caseonit.com>
2019-12-22 12:41:30 +0100

    add rss

---
+++ src/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+export default {
+    siteUrl: 'http://localhost:3000',
+    blogUrl: 'http://localhost:3000/blog',
+    blogTitle: 'Blog de JA Samitier',
+    author: 'JA Samitier',
+    email: 'contacto [at] jasamitier [.] com',
+    social: {
+        twitter: 'https://twitter.com/eckelon',
+        github: 'https://github.com/eckelon',
+        linkedin: 'https://www.linkedin.com/in/jesús-ángel-samitier-05451296',
+    }
+};
---
+++ src/routes/rss.js
@@ -0,0 +1,34 @@
+import { posts } from './blog/_posts';
+import config from '../config'
+const { blogTitle, blogUrl } = config;
+
+const renderFeed = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+<channel>
+    <title><![CDATA[${blogTitle}]]></title>
+    <link>${blogUrl}</link>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+      ${posts.map(({ title, slug, description, date, html }) => `
+          <item>
+              <title>${title}</title>
+        <link>${blogUrl}/${slug}</link>
+        <guid isPermaLink="false">${blogUrl}/${slug}</guid>
+              <description><![CDATA[${description || ''}]]></description>
+              <pubDate>${ date ? new Date(date).toUTCString() : ''}</pubDate>
+              <content:encoded><![CDATA[${html || ''}]]></content:encoded>
+          </item>
+      `).join('\n')}
+</channel>
+</rss>`;
+
+export function get(req, res) {
+
+  res.writeHead(200, {
+    'Cache-Control': `max-age=0, s-max-age=${3600}`,
+    'Content-Type': 'application/rss+xml;charset=UTF-8'
+  });
+
+  const feed = renderFeed(posts);
+  res.end(feed);
+
+}
